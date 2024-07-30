2a6c45995c5fa9a2a59adbd39bb66444e3c2a1a7
blalshaheen224 <blalsheen2245852@gmail.com>
2024-07-30 20:55:35 +0200

    basic.php

---
+++ basic.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> الصفحة الرىيسة</title>
+    <style>
+        a{
+             padding: 15px;
+             text-align:center;
+             text-decoration: none; 
+             font-family: "Cairo", sans-serif;   
+             font-weight: black;
+             width:100px ;
+             margin-top::1px;
+             font-weight: bold;
+             font-size: 1000%;
+             color: silver ;
+        }
+        h1
+        {
+            color: black ;
+            font-weight: bold;
+            font-family: "Cairo", sans-serif;   
+            margin: 20px;
+
+        }
+    </style>
+</head>
+<body>
+  
+    <center>
+    <h1>الصفحة الرىيسة</h1>
+    <a href="index.php">admin</a>
+    <br>
+    <a href="shop.php">user</a>
+    </center>
+</body>
+</html>
